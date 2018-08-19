3601341ee5801837eaa087c38bfbf617e619101c
roxyrobert <robert.ssebintu@gmail.com>
2018-08-19 23:17:22 +0300

    Add test for add_question endpoint

---
+++ tests/test_api.py
@@ -0,0 +1,29 @@
+import unittest
+import json
+from api.views import app
+
+class QuestionTestCase(unittest.TestCase):
+    def setUp(self):
+        self.sample_data = {
+            'body':'What is HTML in full',
+            'poster':'Robert'
+        }
+
+        # initialize the test client
+        self.client = app.test_client()
+
+    def test_add_question(self):
+        response = self.client.post(
+            '/api/v1/questions',
+            data=json.dumps(self.sample_data),
+            content_type='application/json'
+        )
+        resp_data = json.loads(response.data.decode())
+        # assert keys
+        self.assertIn('status', resp_data)
+        self.assertIn('message', resp_data)
+        self.assertIn('_id', resp_data)
+        # assert expected data
+        self.assertEqual(resp_data['status'],'ok')
+        # assert expected status code
+        self.assertEqual(response.status_code,201)
